3890ea589f2fda1c9923de2b9c5ed4932f9bd2c2
shukun xiao <huzeyu130@gmail.com>
2022-04-14 14:02:38 +0800

    iteration1

---
+++ wp-content/plugins/content-protector/vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '3650ea78e4e25dfffc9830de0fe2bf8375ba16a0',
+    'name' => 'patrickposner/passster',
+  ),
+  'versions' => 
+  array (
+    'patrickposner/passster' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '3650ea78e4e25dfffc9830de0fe2bf8375ba16a0',
+    ),
+  ),
+);
---
+++ wp-content/plugins/delicious-recipes/vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '0b5ef80aeb17bcb9262d361a986f0819b9c0ffd6',
+    'name' => 'xqluz/delicious-recipes',
+  ),
+  'versions' => 
+  array (
+    'xqluz/delicious-recipes' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '0b5ef80aeb17bcb9262d361a986f0819b9c0ffd6',
+    ),
+  ),
+);
